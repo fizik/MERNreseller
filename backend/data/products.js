afb6f6323108ce0f51aade73efcdcb1a7139cc5d
codevibek <vibek2925@gmail.com>
2021-07-15 20:26:44 +0545

    Commited Halfway

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: "1",
     name: "Airpod",
     image: "/images/airpods.jpg",
     description: "blah blah blah",
@@ -12,16 +11,26 @@ const products = [
     numReviews: 4,
   },
   {
-    _id: "2",
     name: "Banana",
     image: "/images/airpods.jpg",
     description: "blah blah blah",
     brand: "Apple",
-    category: "Electronics",
-    price: 89.99,
-    countInStock: 3,
+    category: "Fruit",
+    price: 9.99,
+    countInStock: 1,
     rating: 4,
-    numReviews: 4,
+    numReviews: 3,
+  },
+  {
+    name: "Bat",
+    image: "/images/airpods.jpg",
+    description: "blah blah blah",
+    brand: "Cricket",
+    category: "sports",
+    price: 19.99,
+    countInStock: 11,
+    rating: 3.5,
+    numReviews: 7,
   },
 ];
 export default products;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homescreen from "./screens/Homescreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./bootstrap.min.css";
@@ -16,6 +17,7 @@ function App() {
         <Container>
           <Route path="/" component={Homescreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
